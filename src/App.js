658b30c1973751e5428c664e6d19845b56137de1
Diegocarrero <diegocar2413@gmail.com>
2023-02-05 18:03:08 -0500

    Complete almost all the tests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App () {
+  const [score, setScore] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  function changeScore() {
+    setScore(score + increment);
+    if (score >= 100) {
+      return (
+        <div>
+          <h2>You Win!</h2>
+          <button onClick={restart}>Play again?</button>
+        </div>
+      )
+    }
+  }
+
+  function changeIncrement() {
+    if (score < 10) {
+      window.alert(`You can't afford that!`);
+    } else {
+      setIncrement(increment + 1);
+      setScore(score - 10);
+    }
+  }
+
+  function restart() {
+
+  }
+
+  if (score >= 100) {
     return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+      <div>
+        <h2>You Win!</h2>
+        <button onClick={restart}>Play again?</button>
+      </div>
+    )
+  }
+
+  return (
+    <main>
+      <h2>Current Score: {score}</h2>
+      <button onClick={changeScore}>+{increment}</button>
+      <button onClick={changeIncrement}>Pay 10 points to change from +{increment} to +{increment + 1}</button>
+    </main>
+  );
 }
+
 
 export default App;
