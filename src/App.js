5ac4a7db5221714352dc2cec2cc90ab057243999
BertCasale <bthebest55@gmail.com>
2023-02-05 16:30:25 -0500

    general layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
+
 
 function App () {
+
+
+
+
+    
     return (
       <main>
-        <p>React State Lab</p>
+        <h1>Current Score:{}</h1>
+        <button>+{}</button>
+        <br></br>
+        <button>Pay 10 points to change from +{} to +{}</button>
       </main>
     );
 }
