e287600686ed68eafb7768d3e0419bfa4a7d1958
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-02-05 18:13:45 -0500

    functional however needs tests to pass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
+import { useState } from "react";
 import React from "react";
 import "./App.css";
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [score, setScore] = useState(0);
+  const [number, setNumber] = useState(1);
+  const [toggleWin, setToggleWin] = useState(false);
+
+  function scoreUp() {
+    setScore(score + number);
+    if (score > 100) {
+      setToggleWin(!toggleWin);
+    }
+  }
+
+  function numberUp() {
+    if (score > 9) {
+      setScore(score - 10);
+      setNumber(number + 1);
+    } else {
+      alert("You can't afford that!");
+    }
+  }
+
+  function playAgain() {
+    setScore(0);
+    setNumber(1);
+    setToggleWin(false);
+  }
+
+  return (
+    <main>
+      <h1 onClick={scoreUp}>Current Score: {score}</h1>
+      <button
+        style={toggleWin ? { display: "none" } : { display: "block" }}
+        onClick={scoreUp}
+        value={number}
+      >
+        +{number}
+      </button>
+      {/* must throw an Alert if value is less than 10 points */}
+      <button
+        style={toggleWin ? { display: "none" } : { display: "block" }}
+        onClick={numberUp}
+      >
+        Pay 10 points to change from +{number} to {number + 1}
+      </button>
+      <h2 style={toggleWin ? { display: "block" } : { display: "none" }}>
+        You Win
+      </h2>
+      <button
+        style={toggleWin ? { display: "block" } : { display: "none" }}
+        onClick={playAgain}
+      >
+        Play again?
+      </button>
+    </main>
+  );
 }
 
 export default App;
