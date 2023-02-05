abc6cf3378dd5dd7b3b3c255bf9774ad61c68486
JoseC620 <Jcepeda6201@gmail.com>
2023-02-05 17:59:57 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App () {
+
+  const [counter, setCounter] = useState(0);
+  const [current, setCurrent] = useState(1);
+
+  function game(){
+    setCounter(counter + current);
+    }
+
+  function refresh(){
+    setCounter(0)
+    setCurrent(1)
+  }
+
+  function tenPoints(){
+   if(counter < 10){
+    alert("You can't afford that!")
+   } else if (counter >= 10){
+   setCounter(counter -10)
+   setCurrent(current + 1)
+   }
+  }
+  if(counter < 100){
     return (
       <main>
-        <p>React State Lab</p>
+        <div>
+        <h3>Current Score: {counter}</h3>
+        <button onClick={game}> +{current} </button>
+        <br></br>
+        <button onClick={tenPoints}>Pay 10 points to change from +{current} to +{current + 1}</button>
+        </div>
       </main>
     );
+  } else if (counter >= 100){
+    return (
+    <main>
+      <div>
+        <h1>{counter}</h1>
+        <h2>You Win!</h2>
+        <button onClick={refresh}>Play again?</button>
+      </div>
+    </main>
+    )
+  }
 }
 
 export default App;
