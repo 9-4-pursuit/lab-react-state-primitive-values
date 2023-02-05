f88542119a819b3db8a1344f516818e59efe357c
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-02-05 17:01:10 -0500

    lab-react-state-primitive-values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState}from "react";
 import "./App.css";
 
 function App () {
+   const[score, setSCore] = useState(0);
+   const [increment, setIncrement] = useState(1)
+
+   function changeScore() {
+    setSCore(score + increment);
+   }
     return (
       <main>
-        <p>React State Lab</p>
+        <h1>Current Score: {score}</h1>
+        <button onClick={changeScore}> +{increment}</button>
       </main>
     );
 }
