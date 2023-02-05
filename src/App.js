218af553ccd65263ae6aa48cc5fbc7284c517aed
BertCasale <bthebest55@gmail.com>
2023-02-05 17:21:40 -0500

    lab complete

--- src/App.js
+++ src/App.js
@@ -5,18 +5,47 @@ import { useState } from "react";
 
 function App () {
 
+    const [counter, setCounter] = useState(0)
+    const [i, setI] = useState(1)
 
+    function increasePoints() {
+        setCounter(counter + i);
+    }
 
+    function payPoints() {
+        
+        if (counter >= 10) {
+            setI(i+1);
+            setCounter(counter - 10);
+        } else { 
+            window.alert("You can't afford that!")
+        }
+    }
+
+    function playAgain(){
+        setCounter(0);
+        setI(1);
+    }
 
     
-    return (
-      <main>
-        <h1>Current Score:{}</h1>
-        <button>+{}</button>
-        <br></br>
-        <button>Pay 10 points to change from +{} to +{}</button>
-      </main>
-    );
+    if (counter >= 100) {
+        return (
+            <main>
+                <h1>You Win!</h1>
+                <button onClick={playAgain}>Play again?</button>
+            </main>
+        )
+    } else {
+        return (
+            <main>
+                <h1>Current Score: {counter}</h1>
+                <button onClick={increasePoints}>+{i}</button>
+                <br></br>
+                <button onClick={payPoints}>Pay 10 points to change from +{i} to +{i + 1}</button>
+            </main>
+        )
+    }
+    
 }
 
 export default App;
