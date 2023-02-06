1203d69da030cc1d51d7949af51020cb84d4de3c
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-02-06 08:57:46 -0500

    lab-react-state-primitive-values

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
-import { useState } from 'react';
+import React, { useState } from "react";
 import "./App.css";
 
 function App () {
   const [score, setScore] = useState(0);
-  const [increment, setIncrement] = useState(1)
-  
+  const [increment, setIncrement] = useState(1);
+
   function changeScore() {
-    setScore(score + increment)
-   }
-
-  function changeIncrements() {
-    if(score < 10) {
-     window.alert(`You can't afford that!`)
-   } else {
-     setIncrement(increment + 1)
-     setScore(score - 10)
-    }
-   }
-
-   function resetCounter() {
-       setIncrement(1)
-       setScore(0);
-     } 
-   
-  
- if(score < 100){
-  return (
-    
-     <main>
-       <h2>Current Score: {score}</h2>
-       <button onClick={changeScore}>+{increment}</button>
-       <button onClick={changeIncrements}>Pay 10 points to change from +{increment} to +{increment + 1}</button>
-     </main>
-     )
-  
-   } else {
+    setScore(score + increment);
+    if (score >= 100) {
       return (
-     <main>
-       <div>
-         <h2>You Win!</h2>
-         <button onClick={resetCounter}>Play again?</button>
-       </div>
-     </main>
-   )
+        <div>
+          <h2>You Win!</h2>
+          <button onClick={restart}>Play again?</button>
+        </div>
+      )
+    }
   }
-   
+
+  function gameOver () {
+    setNewScore(0);
+    setNumber(1)
+
+  }
+
+  function changeIncrement() {
+    if (score < 10) {
+      window.alert(`You can't afford that!`);
+    } else {
+      setIncrement(increment + 1);
+      setScore(score - 10);
+    }
+  }
+
+  function restart() {
+
+  }
+
+  if (score >= 100) {
+    return (
+      <div>
+        <h2>You Win!</h2>
+        <button onClick={restart}>Play again?</button>
+      </div>
+    )
+  }
+
+  return (
+    <main>
+      <h2>Current Score: {score}</h2>
+      <button onClick={changeScore}>+{increment}</button>
+      <button onClick={changeIncrement}>Pay 10 points to change from +{increment} to +{increment + 1}</button>
+    </main>
+  );
 }
 
-default export App;
+
+export default App;
