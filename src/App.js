688ce939c4ac5815218d5c640cfadf5f9212cf7f
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-02-05 21:21:11 -0500

    lab-react-state-p rimitive-values

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
-import React, {useState}from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App () {
-   const[score, setScore] = useState(0);
-   const [increment, setIncrement] = useState(1)
-  
+function App() {
+  const [score, setScore] = useState(0);
+  const [increment, setIncrement] = useState(1);
 
-   function changeScore() {
+  function changeScore() {
     setScore(score + increment);
-    if (score >=100) {
+    if (score >= 100) {
       return (
         <div>
           <h2>You Win!</h2>
+          <button onClick={restart}> Play again?</button>
+        </div>
+      );
+    }
+  }
+
+  function changeIncrements() {
+    if (score < 10) {
+      window.alert(`You can't afford that!`);
+    } else {
+      setIncrement(increment + 1);
+      setScore(score - 10);
+    }
+  }
+
+    function restart(){
+
+    }
+
+    if(score >= 100) {
+      return (
+        <div>
+          <h2> You Win!</h2>
+          <button onClick={resturant}>Play again ?</button>
         </div>
       )
     }
-   }
 
-   function byIncrements() {
-    if (score < 10){
-      window.alert("You can't afford that!")
-     } else {
-      setIncrement(increment + 1);
-      setScore(score - 10);
-     }
-    }
-
-    return (
-      <main>
-        <h1>Current Score: {score}</h1>
-        <button onClick={changeScore}> +{increment}</button>
-        <button onClick={byIncrements}> {increment + 1}Pay 10 points to change from 1 to 2</button>
-      </main>
-    );
+  return (
+    <main>
+      <h1>Current Score: {score}</h1>
+      <button onClick={changeScore}> +{increment}</button>
+      <button onClick={byIncrements}>
+        {" "}
+        {increment + 1}Pay 10 points to change from 1 to 2
+      </button>
+    </main>
+  );
 }
 
 export default App;
