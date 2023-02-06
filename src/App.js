fbe792d85529234393e5de329ed7ce1c03e6b859
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-02-05 21:07:04 -0500

    lab-react-state-primitive-values

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import React, {useState}from "react";
 import "./App.css";
 
 function App () {
-   const[score, setSCore] = useState(0);
+   const[score, setScore] = useState(0);
    const [increment, setIncrement] = useState(1)
+  
 
    function changeScore() {
-    setSCore(score + increment);
+    setScore(score + increment);
    }
+
+   function byIncrements() {
+    if (score < 10){
+      window.alert("You can't afford that!")
+    } 
+    
+   }
+
     return (
       <main>
         <h1>Current Score: {score}</h1>
         <button onClick={changeScore}> +{increment}</button>
+        <button onClick={byIncrements}> {increment + 1}Pay 10 points to change from 1 to 2</button>
       </main>
     );
 }
