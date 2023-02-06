2f21c939bfe9e06a0a50077453b187089a1d867f
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-02-06 09:03:31 -0500

    lab-react-state-primitive-values

--- src/App.js
+++ src/App.js
@@ -1,58 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App () {
   const [score, setScore] = useState(0);
-  const [increment, setIncrement] = useState(1);
-
+  const [increment, setIncrement] = useState(1)
+  
   function changeScore() {
-    setScore(score + increment);
-    if (score >= 100) {
-      return (
-        <div>
-          <h2>You Win!</h2>
-          <button onClick={restart}>Play again?</button>
-        </div>
-      )
+    setScore(score + increment)
+   }
+
+  function changeIncrements() {
+    if(score < 10) {
+     window.alert(`You can't afford that!`)
+   } else {
+     setIncrement(increment + 1)
+     setScore(score - 10)
     }
-  }
+   }
 
-  function gameOver () {
-    setNewScore(0);
-    setNumber(1)
-
-  }
-
-  function changeIncrement() {
-    if (score < 10) {
-      window.alert(`You can't afford that!`);
-    } else {
-      setIncrement(increment + 1);
-      setScore(score - 10);
-    }
-  }
-
-  function restart() {
-
-  }
-
-  if (score >= 100) {
-    return (
-      <div>
-        <h2>You Win!</h2>
-        <button onClick={restart}>Play again?</button>
-      </div>
-    )
-  }
-
+   function resetCounter() {
+       setIncrement(1)
+       setScore(0);
+     } 
+   
+  
+ if(score < 100){
   return (
-    <main>
-      <h2>Current Score: {score}</h2>
-      <button onClick={changeScore}>+{increment}</button>
-      <button onClick={changeIncrement}>Pay 10 points to change from +{increment} to +{increment + 1}</button>
-    </main>
-  );
+    
+     <main>
+       <h2>Current Score: {score}</h2>
+       <button onClick={changeScore}>+{increment}</button>
+       <button onClick={changeIncrements}>Pay 10 points to change from +{increment} to +{increment + 1}</button>
+     </main>
+     )
+  
+   } else {
+      return (
+     <main>
+       <div>
+         <h2>You Win!</h2>
+         <button onClick={resetCounter}>Play again?</button>
+       </div>
+     </main>
+   )
+  }
+   
 }
-
 
 export default App;
