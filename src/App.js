5fd42c9689e56071f49e7543087b5c75f05d16a6
pjungjs <pjungjs@gmail.com>
2023-02-05 19:55:57 -0500

    cleaned up the code. realized that I don't need the third useState with boolean value.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./App.css";
 function App() {
   const [score, setScore] = useState(0);
   const [point, setPoint] = useState(1);
-  const [visible, setVisible] = useState(true);
 
   function updateScore() {
     setScore(score + point);
@@ -23,30 +22,25 @@ function App() {
   function playAgain() {
     setScore(0);
     setPoint(1);
-    setVisible(!visible);
   }
 
-  if(visible) {
-    return(
-      <div className="App">
-        <h1>Current Score: {score}</h1>
-        <div className="play" style={{visibility: "visible"}}>
-          <button onClick={score < 100 ? updateScore : setVisible(!visible)}>+{point}</button>
+  return (
+    <div className="App">
+      { score < 100 ? (
+        <div className="play">
+          <h1>Current Score: {score}</h1>
+          <button onClick={updateScore}>+{point}</button>
           <br></br>
           <button onClick={updatePoint}>Pay 10 points to change from +{point} to +{point+1}</button>  
         </div>
-      </div>
-    )
-  } else {
-    return(
-      <div className="App">
-        <div className="won" style={{visibility: "visible"}}>
+      ) : (
+        <div className="won">
           <h2>You Win!</h2>
           <button onClick={playAgain}>Play again?</button>
         </div>
-      </div>
-    )
-  }
+      ) }
+    </div>
+  )
 }
 
 export default App;
