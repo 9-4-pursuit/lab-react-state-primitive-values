83d5929035e6fd4f68008ca3d05449989b2a3195
Felizj17 <felizj17@gmail.com>
2023-02-08 23:13:12 -0500

    lab complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react'
+import './App.css'
 
-function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+function App() {
+  const [points, setPoints] = useState(0)
+  const [win, setWin] = useState(false)
+  const [multiplier, setMultiplier] = useState(1)
+  const increment = () => {
+    setPoints(points + multiplier)
+    if (points+multiplier >= 100) {
+      setWin(!win)
+    }
+  }
+  const upgrade = () => {
+    if (points < 10) {
+      alert("You can't afford that!")
+    } else {
+      setMultiplier(multiplier + 1)
+      setPoints(points - 10)
+    }
+  }
+  const reset=()=>{
+    setWin(!win)
+    setPoints(0)
+    setMultiplier(1)
+  }
+  return (
+    <main>
+      {win ? (
+        <>
+          <h2>You Win!</h2>
+          <p>{points}</p>
+          <button onClick={reset}>Play again?</button>
+        </>
+      ) : (
+        <>
+          <p>Current Score: {points} </p>
+          <button onClick={increment}>+{multiplier}</button>
+          <button onClick={upgrade}>
+            Pay 10 points to change from +{multiplier} to +{multiplier+1}
+          </button>
+        </>
+      )}
+    </main>
+  )
 }
 
-export default App;
+export default App
