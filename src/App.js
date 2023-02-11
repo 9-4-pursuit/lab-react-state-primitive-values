ff67f96b354cf9534e8086671eb597733d58362f
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-02-11 01:35:11 -0500

    set useState, and increments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App () {
+  const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  const incrementCount = () => {
+    setCount(count + increment) //former useState + increased increment
+  }
+
     return (
       <main>
-        <p>React State Lab</p>
+        {/* <p>React State Lab</p> */}
+        <p>Current Score: {count}</p>
+        <button onClick={incrementCount}>+{increment}</button>
+        <br></br>
       </main>
     );
 }
