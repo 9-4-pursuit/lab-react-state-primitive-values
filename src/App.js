432f9055d73aa560628a07e5d12846b78cea25f4
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-02-11 02:21:36 -0500

    completed lab

--- src/App.js
+++ src/App.js
@@ -9,14 +9,48 @@ function App () {
     setCount(count + increment) //former useState + increased increment
   }
 
+    const payTen = () => {
+      if (count < 10) {
+        alert("You can't afford that!");
+        } else {
+          setCount(count-10)
+          setIncrement(increment + 1)
+      }
+    }
+
+    function playAgain() {
+      setCount(0);
+      setIncrement(1);
+    }
+
+    if (count < 100) {
     return (
       <main>
         {/* <p>React State Lab</p> */}
-        <p>Current Score: {count}</p>
+        <p>
+          <h2 id="score">Current Score: {count}
+          </h2>
+        </p>
         <button onClick={incrementCount}>+{increment}</button>
-        <br></br>
+        <p>
+        <button onClick = {payTen}>Pay 10 points to change from +{increment} to +{increment+1}</button>
+        </p>
       </main>
     );
+    } else {
+      return (
+        <main>
+          {/* <p>
+            Current Score: {count}
+            </p> */}
+          <h2>
+            You Win!
+            </h2>
+            <p></p>
+            <button onClick={playAgain}>Play again?</button>
+        </main>
+    );
+  }
 }
 
 export default App;
