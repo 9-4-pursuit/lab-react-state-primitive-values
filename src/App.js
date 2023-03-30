077a0da222fd6f3d49189502e34e0ce92ac4fc35
Ari-So-Irie <arius1500@gmail.com>
2023-03-30 13:21:38 -0400

    Arius' React State Primitive Values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+  const [score, setScore] = useState(0);
+  const [increment, setIncrement] = useState(1)
+  
+  function changeScore() {
+    setScore(score + increment)
+   }
+
+  function changeIncrements() {
+    if(score < 10) {
+     window.alert(`You can't afford that!`)
+   } else {
+     setIncrement(increment + 1)
+     setScore(score - 10)
+    }
+   }
+
+   function resetCounter() {
+       setIncrement(1)
+       setScore(0);
+     } 
+   
+  
+ if(score < 100){
+  return (
+    
+     <main>
+       <h2>Current Score: {score}</h2>
+       <button onClick={changeScore}>+{increment}</button>
+       <button onClick={changeIncrements}>Pay 10 points to change from +{increment} to +{increment + 1}</button>
+     </main>
+     )
+  
+   } else {
+      return (
+     <main>
+       <div>
+         <h2>You Win!</h2>
+         <button onClick={resetCounter}>Play again?</button>
+       </div>
+     </main>
+   )
+  }
+   
 }
 
 export default App;
